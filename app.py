7ccd11ffe706315b0e705c666d15483b41fd298b
GatoMontes002 <gatomontes0000@gmail.com>
2024-11-21 08:38:03 -0300

    renombrado de archivos build a html

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
-
+freezer = Freezer(app)
 @app.route('/')
 def index():
     return render_template('index.html')
